ff6fd11a1e265c31ca1e0a12ca514cfb45856ab1
pjmoo <00pjmoooo@gmail.com>
2026-08-12 10:47:41 +0900

    feat: 회원가입 DTO, 컨트롤러 바인딩 및 뷰(join.html) 작성

--- src/main/java/org/example/sec_crud/controller/UserAccountController.java
+++ src/main/java/org/example/sec_crud/controller/UserAccountController.java
@@ -1,9 +1,15 @@
 package org.example.sec_crud.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.sec_crud.domain.dto.UserJoinFormDTO;
 import org.example.sec_crud.service.UserAccountService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,7 +24,19 @@ public class UserAccountController {
     }
 
     @GetMapping("/join")
-    public String join() {
+    public String join(Model model) {
+        model.addAttribute("form", new UserJoinFormDTO("", ""));
         return "user/join";
+    }
+
+    @PostMapping("/join")
+    public String join(
+            @Validated @ModelAttribute("form") UserJoinFormDTO form,
+            BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "user/join";
+        }
+//        userAccountService.join(form);
+        return "redirect:/user/login";
     }
 }
---
+++ src/main/java/org/example/sec_crud/domain/dto/UserJoinFormDTO.java
@@ -0,0 +1,9 @@
+package org.example.sec_crud.domain.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record UserJoinFormDTO(
+        @NotBlank @Size(min = 4, max = 8) String username,
+        @NotBlank @Size(min = 8, max = 20) String password) {
+}
