c4489abe7de69557f4e77b717f7d7131ed2bb545
pjmoo <00pjmoooo@gmail.com>
2026-08-12 09:22:37 +0900

    Initial commit

---
+++ src/main/java/org/example/sec_crud/SecCrudApplication.java
@@ -0,0 +1,13 @@
+package org.example.sec_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/sec_crud/SecCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sec_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
